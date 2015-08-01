144f956db2303aec390ca9d54b236a044a06bbf7
Guillaume EHRET <gui.ehret@gmail.com>
2015-08-01 09:04:26 +0200

    Fix bug to be able to launch the webapp with url localhost:8080

---
+++ server/src/main/java/org/mixit/cesar/CesarConfig.java
@@ -0,0 +1,10 @@
+package org.mixit.cesar;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class CesarConfig extends WebMvcConfigurerAdapter {
+
+
+}
--- server/src/main/java/org/mixit/cesar/web/MemberController.java
+++ server/src/main/java/org/mixit/cesar/web/MemberController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/member")
+@RequestMapping("/api/member")
 public class MemberController {
 
     @Autowired
