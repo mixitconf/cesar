f67c97f31ce892b24f849b42e830b03e26f12e2b
Guillaume EHRET <gui.ehret@gmail.com>
2015-09-12 23:07:12 +0200

    Fixes pb on speaker filter

--- server/src/main/java/org/mixit/cesar/repository/MemberRepository.java
+++ server/src/main/java/org/mixit/cesar/repository/MemberRepository.java
@@ -24,7 +24,7 @@ public interface MemberRepository extends CrudRepository<Member, Long> {
     @Query(value = "SELECT DISTINCT m FROM Member m left join fetch m.sessions s  left join fetch m.events e left join fetch m.interests i left join fetch m.sharedLinks l where e.id = :idEvent")
     List<Member> findAllSpeakers(@Param("idEvent") Long idEvent);
 
-    @Query(value = "SELECT DISTINCT m FROM Member m left join fetch m.sessions s left join fetch m.events e left join fetch m.interests i left join fetch m.sharedLinks l where e.id = :idEvent and s.sessionAccepted = true")
+    @Query(value = "SELECT DISTINCT m FROM Member m left join fetch m.sessions s left join fetch s.event e left join fetch m.interests i left join fetch m.sharedLinks l where e.id = :idEvent and s.sessionAccepted = true and s.format <> 'LightningTalk'")
     List<Member> findAllAcceptedSpeakers(@Param("idEvent") Long idEvent);
 
     @Query(value = "SELECT DISTINCT m FROM Sponsor m left join fetch m.events e left join fetch m.interests i left join fetch m.sharedLinks l where e.id = :idEvent")
