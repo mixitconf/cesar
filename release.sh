82f9e5dc8df8573c959f6dc9c26ee938f5e04679
Guillaume EHRET <guillaume@dev-mind.fr>
2015-11-05 12:09:16 +0100

    Adds release plugin

---
+++ release.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+cd `dirname "$0"`
+
+if [ $# -eq 2 ]; then
+	releaseVersion=$1
+	newVersion=$2
+else
+	echo "Bad number of arguments => release.sh <releaseVersion> <newVersion>"
+	exit 1
+fi
+
+./gradlew clean release -Prelease.useAutomaticVersion=true -PreleaseVersion=${releaseVersion} -PnewVersion=${newVersion}
+
+
