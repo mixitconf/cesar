e2e8b6d44af2c17633b6acbc4c190111c86f3e2e
Guillaume EHRET <gui.ehret@gmail.com>
2015-11-26 18:20:26 +0100

    Use HTML special characters in mails

--- server/src/main/java/org/mixit/cesar/security/service/mail/MailBuilder.java
+++ server/src/main/java/org/mixit/cesar/security/service/mail/MailBuilder.java
@@ -38,13 +38,13 @@ public class MailBuilder {
         switch (typeMail) {
             case REINIT_PASSWORD:
                 url = String.format("%s/app/account/password?token=%s", urlFactory.getBaseUrl(), account.getToken());
-                message.append("<p>Vous nous avez demandé de réinitialiser votre mot de passe. Pour celà veuillez suivre le lien suivant <a href=\"")
+                message.append("<p>Vous nous avez demand&nbsp; de r&nbsp;initialiser votre mot de passe. Pour celà veuillez suivre le lien suivant <a href=\"")
                         .append(url)
                         .append("\">")
                         .append(url)
                         .append("</a></p><p>Sur cette page vous devrez renseigner ce mot de passe provisoire <b>")
                         .append(account.getPassword())
-                        .append("</b> pour pouvoir en définir un nouveau. ")
+                        .append("</b> pour pouvoir en d&nbsp;finir un nouveau. ")
                         .append("Si vous n'êtes pas à l'origine de cette action veuillez nous tenir informer</p>");
                 break;
             case EMAIL_CHANGED:
@@ -58,7 +58,7 @@ public class MailBuilder {
                         .append("</a></p>");
                 break;
             case ACCOUND_NEW_VALIDATION:
-                message.append("<p>Vous nous avez demandé de réinitialiser votre mot de passe mais vous utilisez une connexion via le reseau social ")
+                message.append("<p>Vous nous avez demand&nbsp; de r&nbsp;initialiser votre mot de passe mais vous utilisez une connexion via le reseau social ")
                         .append(provider.orElse(OAuthProvider.TWITTER))
                         .append(". Nous ne pouvons pas changer le mot de passe. Si vous souhaitez à nouveau valider votre adresse email cliquez sur le lien suivant <a href=\"")
                         .append(url)
@@ -68,17 +68,17 @@ public class MailBuilder {
                 break;
             case CESAR_ACCOUNT_VALIDATION:
             case SOCIAL_ACCOUNT_VALIDATION:
-                message.append("<p>Vous venez de créer un compte sur le site de <a href=\"")
+                message.append("<p>Vous venez de cr&nbsp;er un compte sur le site de <a href=\"")
                         .append(urlFactory.getBaseUrl())
                         .append("\">Mix-IT</a>. Pour le valider veuillez cliquer sur ce lien <a href=\"")
                         .append(url)
                         .append("\">")
                         .append(url)
-                        .append("</a>. Vous pourrez ensuite utiliser la partie sécurisée du site.</p>");
+                        .append("</a>. Vous pourrez ensuite utiliser la partie s&nbsp;curis&nbsp;e du site.</p>");
                 break;
         }
 
-        message.append("<p>Attention, ce code n'est valable que pendant trois heures. Passé ce délai, vous devrez soumettre une nouvelle demande de modification de vos informations d'identification.</p>");
+        message.append("<p>Attention, ce code n'est valable que pendant trois heures. Pass&nbsp; ce d&nbsp;lai, vous devrez soumettre une nouvelle demande de modification de vos informations d'identification.</p>");
         message.append("<b>La team Mix-IT</b>");
         message.append("</div>");
 
