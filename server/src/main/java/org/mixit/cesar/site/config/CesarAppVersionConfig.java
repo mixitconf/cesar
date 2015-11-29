cbd37788f47311291de2b33fb503dade75208ec7
Guillaume EHRET <gui.ehret@gmail.com>
2015-11-29 22:13:55 +0100

    The version number is only checked in cloud profile

--- server/src/main/java/org/mixit/cesar/site/config/CesarAppVersionConfig.java
+++ server/src/main/java/org/mixit/cesar/site/config/CesarAppVersionConfig.java
@@ -1,10 +1,12 @@
 package org.mixit.cesar.site.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @PropertySource(value= "classpath:version.yml")
+@Profile("cloud")
 public class CesarAppVersionConfig {
 
 }
