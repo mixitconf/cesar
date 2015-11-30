5c791fc44f712b92a16765a10bb81c2241f8a348
Guillaume EHRET <guillaume@dev-mind.fr>
2015-11-30 10:57:01 +0100

    The Google Drive docs Ids are now defined like parameters

--- server/src/main/java/org/mixit/cesar/site/web/api/LeafletRedirect.java
+++ server/src/main/java/org/mixit/cesar/site/web/api/LeafletRedirect.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import javax.servlet.http.HttpServletResponse;
 
 import org.mixit.cesar.site.model.session.SessionLanguage;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,33 +15,46 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class LeafletRedirect {
 
+    @Value("${drive.sponsor.form.fr}")
+    private String sponsorFormFr;
+    @Value("${drive.sponsor.form.en}")
+    private String sponsorFormEn;
+    @Value("${drive.sponsor.leaflet.fr}")
+    private String sponsorLeafletFr;
+    @Value("${drive.sponsor.leaflet.en}")
+    private String sponsorLeafletEn;
+    @Value("${drive.speaker.leaflet.fr}")
+    private String speakerLeafletFr;
+    @Value("${drive.speaker.leaflet.en}")
+    private String speakerLeafletEn;
+
     @RequestMapping(value = "/docs/sponsor/form/{language}")
     public void sponsorForm(@PathVariable("language") String language, HttpServletResponse response) throws IOException {
         if (SessionLanguage.en.equals(language.toLowerCase())) {
-            response.sendRedirect("https://drive.google.com/open?id=0B43BPeaKpurvc1ZCN0xFLXJDSzA");
+            response.sendRedirect("https://drive.google.com/open?id=" + sponsorFormEn);
         }
         else {
-            response.sendRedirect("https://drive.google.com/open?id=0B43BPeaKpurvc1ZCN0xFLXJDSzA");
+            response.sendRedirect("https://drive.google.com/open?id=" + sponsorFormFr);
         }
     }
 
     @RequestMapping(value = "/docs/sponsor/leaflet/{language}")
     public void sponsorLeaflet(@PathVariable("language") String language, HttpServletResponse response) throws IOException {
         if (SessionLanguage.en.equals(language.toLowerCase())) {
-            response.sendRedirect("https://drive.google.com/open?id=0B43BPeaKpurvWTNnZU9WTDdUZ28");
+            response.sendRedirect("https://drive.google.com/open?id=" + sponsorFormFr);
         }
         else {
-            response.sendRedirect("https://drive.google.com/open?id=0B43BPeaKpurvWTNnZU9WTDdUZ28");
+            response.sendRedirect("https://drive.google.com/open?id=" + sponsorFormFr);
         }
     }
 
     @RequestMapping(value = "/docs/speaker/leaflet/{language}")
     public void speakerLeaflet(@PathVariable("language") String language, HttpServletResponse response) throws IOException {
         if (SessionLanguage.en.equals(language.toLowerCase())) {
-            response.sendRedirect("https://drive.google.com/open?id=0B43BPeaKpurvWTNnZU9WTDdUZ28");
+            response.sendRedirect("https://drive.google.com/open?id=" + sponsorFormFr);
         }
         else {
-            response.sendRedirect("https://drive.google.com/open?id=0B43BPeaKpurvWTNnZU9WTDdUZ28");
+            response.sendRedirect("https://drive.google.com/open?id=" + sponsorFormFr);
         }
     }
 }
