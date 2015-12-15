671498e48cfcd436975a698f65b6f44a1c0d18bb
Guillaume EHRET <guillaume@dev-mind.fr>
2015-12-16 00:09:55 +0100

    Update config to be able to start in cloud env

--- server/src/main/java/org/mixit/cesar/site/config/CesarAppVersionConfig.java
+++ server/src/main/java/org/mixit/cesar/site/config/CesarAppVersionConfig.java
@@ -1,5 +1,6 @@
 package org.mixit.cesar.site.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
@@ -9,4 +10,11 @@ import org.springframework.context.annotation.PropertySource;
 @Profile("cloud")
 public class CesarAppVersionConfig {
 
+    /**
+     * @see org.mixit.cesar.CesarInitializer
+     */
+    @Bean
+    public String cesarInitializer(){
+        return "not use in cloud profile";
+    }
 }
