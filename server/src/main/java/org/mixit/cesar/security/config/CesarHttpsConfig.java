6440c814957e96c73927176ec69ba87cf199784e
Guillaume EHRET <guillaume@dev-mind.fr>
2015-12-29 00:36:39 +0100

    Add a custom ServletContainerFactory to automatically redirect user on https

---
+++ server/src/main/java/org/mixit/cesar/security/config/CesarHttpsConfig.java
@@ -0,0 +1,33 @@
+package org.mixit.cesar.security.config;
+
+import org.apache.catalina.Context;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+public class CesarHttpsConfig {
+
+    @Profile("cloud")
+    @Bean
+    @Primary
+    public EmbeddedServletContainerFactory containerCustomizer() {
+        return new TomcatEmbeddedServletContainerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+
+    }
+}
