875dcae51dca2eecd3bfcbf75ed461aead106d8c
Guillaume EHRET <guillaume@dev-mind.fr>
2016-01-15 10:59:10 +0100

    Clear cache after article update

--- server/src/main/java/org/mixit/cesar/site/web/app/ArticleWriterController.java
+++ server/src/main/java/org/mixit/cesar/site/web/app/ArticleWriterController.java
@@ -1,10 +1,14 @@
 package org.mixit.cesar.site.web.app;
 
+import static org.mixit.cesar.site.config.CesarCacheConfig.CACHE_ARTICLE;
+
 import com.fasterxml.jackson.annotation.JsonView;
+import org.mixit.cesar.site.config.CesarCacheConfig;
 import org.mixit.cesar.site.model.FlatView;
 import org.mixit.cesar.site.model.article.Article;
 import org.mixit.cesar.site.repository.ArticleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.CacheManager;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +22,9 @@ public class ArticleWriterController {
 
     @Autowired
     ArticleRepository articleRepository;
+
+    @Autowired
+    CacheManager cacheManager;
 
     @RequestMapping(method = RequestMethod.POST)
     @JsonView(FlatView.class)
@@ -35,6 +42,9 @@ public class ArticleWriterController {
         } else {
             articleSaved = article;
         }
+
+        //TODO something cleaner
+        cacheManager.getCache(CACHE_ARTICLE).clear();
         return articleRepository.save(articleSaved);
     }
 
