22afc6d5dc360ea2e4f647de15c551e5e161f7f0
Guillaume EHRET <guillaume@dev-mind.fr>
2016-01-20 09:06:38 +0100

    ref #192 cfp info on homepage

---
+++ client-ng1/src/app/js/home/cfp.directive.js
@@ -0,0 +1,12 @@
+(function () {
+
+  'use strict';
+  angular.module('cesar-home').directive('cesarHomeCfp', function () {
+    'ngInject';
+
+    return {
+      templateUrl: 'js/home/cfp.directive.html',
+      scope: {}
+    };
+  });
+})();
---
+++ client-ng1/src/app/js/home/video.directive.js
@@ -0,0 +1,12 @@
+(function () {
+
+  'use strict';
+  angular.module('cesar-home').directive('cesarHomeVideo', function () {
+    'ngInject';
+
+    return {
+      templateUrl: 'js/home/video.directive.html',
+      scope: {}
+    };
+  });
+})();
