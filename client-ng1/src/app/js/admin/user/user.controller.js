43be65a9812f4d76826c57c2e5f701d060a6bd95
Guillaume EHRET <guillaume@dev-mind.fr>
2016-01-21 18:22:07 +0100

    Fix error JSHint

--- client-ng1/src/app/js/admin/user/user.controller.js
+++ client-ng1/src/app/js/admin/user/user.controller.js
@@ -1,69 +1,10 @@
 (function () {
 
   'use strict';
-  /*global componentHandler */
 
-  angular.module('cesar-cfp').controller('AdminUserCtrl', function ($http) {
+  angular.module('cesar-cfp').controller('AdminUserCtrl', function () {
     'ngInject';
 
-    var ctrl = this;
-
-    if ($stateParams.id) {
-      $http
-        .get('app/cfp/proposal/' + $stateParams.id)
-        .then(function (response) {
-          ctrl.proposal = response.data;
-          ctrl.check();
-        });
-    }
-
-    ctrl.account = account;
-
-    var i8nKeys = {
-      categories: 'view.cfp.category.',
-      status: 'view.cfp.status.',
-      formats: 'view.cfp.format.',
-      types: 'view.cfp.typeSession.',
-      levels: 'view.cfp.level.',
-      maxAttendees: 'view.cfp.nbattendees.'
-    };
-
-    $http.get('app/cfp/param').then(function (response) {
-      response.data.forEach(function (elt) {
-        ctrl[elt.key] = [];
-        elt.value.forEach(function (param) {
-          ctrl[elt.key][param] = i8nKeys[elt.key] + param;
-        });
-      });
-    });
-
-    ctrl.check = function () {
-      if(ctrl.proposal.id){
-        delete ctrl.warningMessage;
-        delete ctrl.confirm;
-
-        $http
-          .post('app/cfp/proposal/check', angular.copy(ctrl.proposal), {ignoreErrorRedirection: 'ignoreErrorRedirection'})
-          .then(function (response) {
-            ctrl.warningMessage = response.data;
-            refresh();
-          })
-          .catch(function () {
-            ctrl.errorMessage = 'UNDEFINED';
-          });
-      }
-    };
-
-    ctrl.goback = function () {
-      $state.go('cfp');
-    };
-
-
-    function refresh() {
-      $timeout(function () {
-        componentHandler.upgradeAllRegistered();
-      }, 1000);
-    }
 
   });
 })();
