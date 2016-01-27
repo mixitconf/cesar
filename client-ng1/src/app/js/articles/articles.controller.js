849b43a7ebd481b50925a12ef1517f8543391a1e
Guillaume EHRET <guillaume@dev-mind.fr>
2016-01-27 21:02:48 +0100

    Add pagination in article page

--- client-ng1/src/app/js/articles/articles.controller.js
+++ client-ng1/src/app/js/articles/articles.controller.js
@@ -5,11 +5,29 @@
   /**
    * member is resolved in app.js
    */
-  angular.module('cesar-articles').controller('ArticlesCtrl', function (articles) {
+  angular.module('cesar-articles').controller('ArticlesCtrl', function (articles, $scope, $filter) {
     'ngInject';
 
     var ctrl = this;
 
     ctrl.articles = articles;
+
+    ctrl.filter = function(){
+      var articlesFiltered =  $filter('filter')(articles, $scope.search);
+      ctrl.pagination.nbtotal = articlesFiltered ? articlesFiltered.length : 0;
+      ctrl.pagination.pages = parseInt(ctrl.pagination.nbtotal/ctrl.pagination.nbitems);
+      return articlesFiltered;
+    };
+
+    ctrl.pagination = {
+      current: 1,
+      nbitems: 10
+    };
+
+    ctrl.displayItem = function (index){
+      var min = ctrl.pagination.current * ctrl.pagination.nbitems - ctrl.pagination.nbitems;
+      var max = ctrl.pagination.current * ctrl.pagination.nbitems - 1;
+      return index>=min && index<=max;
+    };
   });
 })();
