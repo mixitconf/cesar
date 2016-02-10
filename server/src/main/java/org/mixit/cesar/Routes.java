dc4c3df2547088dc34af1e51a4c5ab1b85605146
Guillaume EHRET <guillaume@dev-mind.fr>
2016-02-10 13:28:39 +0100

    Fix route for admaccounts

--- server/src/main/java/org/mixit/cesar/Routes.java
+++ server/src/main/java/org/mixit/cesar/Routes.java
@@ -11,6 +11,7 @@ public class Routes {
 
     @RequestMapping({
             "/account",
+            "/admaccounts",
             "/admcfp",
             "/admcfptalk/{id:\\w+}",
             "/admarticle/{id:\\w+}",
