a7f81e893394a23a14f98d530492177a12ee6751
mpetitdant <mathieu.petitdant@gmail.com>
2016-02-25 22:45:47 +0100

    refs #228 : rss feed on articles

--- server/src/main/java/org/mixit/cesar/site/web/api/ArticleController.java
+++ server/src/main/java/org/mixit/cesar/site/web/api/ArticleController.java
@@ -1,13 +1,18 @@
 package org.mixit.cesar.site.web.api;
 
 import com.fasterxml.jackson.annotation.JsonView;
+import com.rometools.rome.feed.synd.SyndEntry;
+import com.rometools.rome.feed.synd.SyndFeed;
+import com.rometools.rome.feed.synd.SyndFeedImpl;
+import com.rometools.rome.io.FeedException;
+import com.rometools.rome.io.SyndFeedOutput;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.mixit.cesar.security.model.Role;
-import org.mixit.cesar.security.repository.AccountRepository;
 import org.mixit.cesar.security.service.authentification.CurrentUser;
 import org.mixit.cesar.site.model.FlatView;
 import org.mixit.cesar.site.model.article.Article;
+import org.mixit.cesar.site.model.session.SessionLanguage;
 import org.mixit.cesar.site.repository.ArticleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
@@ -17,6 +22,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Api(value = "Articles",
@@ -25,11 +32,11 @@ import java.util.List;
 @RequestMapping("/api/article")
 public class ArticleController {
 
-    @Autowired
-    private ArticleRepository articleRepository;
+    public static final String MIX_IT_ARTICLE = "https://www.mix-it.fr/article/";
+    public static final String MIX_IT_ARTICLES = "https://www.mix-it.fr/articles";
 
     @Autowired
-    private AccountRepository accountRepository;
+    private ArticleRepository articleRepository;
 
     @Autowired
     private ApplicationContext applicationContext;
@@ -37,7 +44,7 @@ public class ArticleController {
     @RequestMapping("/{id}")
     @ApiOperation(value = "Finds one article", httpMethod = "GET")
     @JsonView(FlatView.class)
-    public ResponseEntity<Article> getArticle(@PathVariable("id") Long id) {
+    public ResponseEntity<Article> getArticlesFeed(@PathVariable("id") Long id) {
         Article result;
         if (isCurrentUserAdmin()) {
             result = articleRepository.findArticleById(id);
@@ -49,6 +56,43 @@ public class ArticleController {
                 .ok()
                 .cacheControl(CacheControl.noCache())
                 .body(result);
+    }
+
+    @RequestMapping("/rss/fr_FR")
+    @ApiOperation(value = "Articles RSS (fr)", httpMethod = "GET")
+    @JsonView(FlatView.class)
+    public String getArticlesRssFr() throws IOException, FeedException {
+        return getArticlesFeed(SessionLanguage.fr);
+    }
+
+    @RequestMapping("/rss/us_US")
+    @ApiOperation(value = "Articles RSS (en)", httpMethod = "GET")
+    @JsonView(FlatView.class)
+    public String getArticlesRssEn() throws IOException, FeedException {
+        return getArticlesFeed(SessionLanguage.en);
+    }
+
+    @JsonView(FlatView.class)
+    public String getArticlesFeed(SessionLanguage language) throws IOException, FeedException {
+        SyndFeed feed = new SyndFeedImpl();
+        feed.setFeedType("rss_2.0");
+
+        feed.setTitle("Mix-IT articles");
+        feed.setLink(MIX_IT_ARTICLES);
+        feed.setDescription(" ");
+
+        List<Article> articles = articleRepository.findAllPublishedArticle();
+
+        List<SyndEntry> entries = new ArrayList<>();
+        feed.setEntries(entries);
+
+        for (Article article : articles) {
+            SyndEntry entry = article.buildRssEntry(language);
+            entries.add(entry);
+        }
+
+        SyndFeedOutput output = new SyndFeedOutput();
+        return output.outputString(feed);
     }
 
     private boolean isCurrentUserAdmin() {
