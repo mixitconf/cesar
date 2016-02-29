8f32480da73ffb4da6e377e0ae43a47291e0530d
mpetitdant <mathieu.petitdant@gmail.com>
2016-02-29 22:31:37 +0100

    refs #243 : backend

--- server/src/main/java/org/mixit/cesar/cfp/model/ProposalVote.java
+++ server/src/main/java/org/mixit/cesar/cfp/model/ProposalVote.java
@@ -28,6 +28,9 @@ public class ProposalVote {
     @ManyToOne(optional = false)
     protected Proposal proposal;
 
+    @Lob
+    private String voteComment;
+
     public Long getId() {
         return id;
     }
@@ -75,5 +78,13 @@ public class ProposalVote {
     @Override
     public int hashCode() {
         return Objects.hash(id);
+    }
+
+    public String getVoteComment() {
+        return voteComment;
+    }
+
+    public void setVoteComment(String voteComment) {
+        this.voteComment = voteComment;
     }
 }
