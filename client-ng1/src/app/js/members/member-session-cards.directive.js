9efb43ecb88289a016996e9761fd762d9bddef59
Guillaume EHRET <guillaume@dev-mind.fr>
2016-03-18 22:36:43 +0100

    We need to see only 2016 sessions

--- client-ng1/src/app/js/members/member-session-cards.directive.js
+++ client-ng1/src/app/js/members/member-session-cards.directive.js
@@ -8,18 +8,21 @@
     return {
       template: '<div><cesar-session-cards sessions="sessions" display="minimal"></cesar-session-cards></div>',
       scope: {
-        idSessions: '=sessions'
+        idSessions: '=sessions',
+        current: '='
       },
       controller: function ($scope) {
         $scope.sessions = [];
-        if($scope.idSessions){
-          $scope.idSessions.forEach(function(id){
-            SessionService.getById(id).then(function(response){
-              $scope.sessions.push(response.data);
+        if ($scope.idSessions) {
+          $scope.idSessions.forEach(function (id) {
+            SessionService.getById(id).then(function (response) {
+              if (!$scope.current || response.data.year === $scope.current){
+                $scope.sessions.push(response.data);
+              }
             });
           });
         }
-        else{
+        else {
           $log.warn('This speaker has no session');
         }
       }
