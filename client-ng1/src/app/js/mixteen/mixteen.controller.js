60371861627f11e7be9bf22a7981174ae94fd367
Guillaume EHRET <guillaume@dev-mind.fr>
2016-03-21 21:59:13 +0100

    Adds Aude in mixteen team

--- client-ng1/src/app/js/mixteen/mixteen.controller.js
+++ client-ng1/src/app/js/mixteen/mixteen.controller.js
@@ -13,7 +13,7 @@
     //    Mireille Delpui : mireille.delpui@gmail.com
     //    Francisco Nogueira : nogueira.frma@yahoo.fr
 
-    $http.post('/api/member/byids', [13,701, 4111, 488, 5582, 14, 48, 33, 7562, 7422])
+    $http.post('/api/member/byids', [13,701, 4111, 488, 5582, 14, 48, 33, 7562, 7422, 7722])
         .then(function (response) {
           ctrl.members = response.data;
         });
