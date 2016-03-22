30c3100717a67670302d07ba4c1808c53695a89d
Guillaume EHRET <guillaume@dev-mind.fr>
2016-03-22 22:18:27 +0100

    Adds Amelie in Mixteen team

--- client-ng1/src/app/js/mixteen/mixteen.controller.js
+++ client-ng1/src/app/js/mixteen/mixteen.controller.js
@@ -7,12 +7,10 @@
 
     var ctrl = this;
 
-    // Manque
-    //    Amelie Cordier : acordier@gmail.com
     //    Mireille Delpui : mireille.delpui@gmail.com
     //    Francisco Nogueira : nogueira.frma@yahoo.fr
 
-    $http.post('/api/member/byids', [13,701, 4111, 488, 5582, 14, 48, 33, 7562, 7422, 7722])
+    $http.post('/api/member/byids', [13,701, 4111, 488, 5582, 14, 48, 33, 7562, 7422, 7722, 7762])
         .then(function (response) {
           ctrl.members = response.data;
         });
