9c86180791785191d65fb8c7583788c72f0a511d
Guillaume EHRET <guillaume@dev-mind.fr>
2016-04-01 23:42:44 +0200

    Adds Mireille in Mixteen team

--- client-ng1/src/app/js/mixteen/mixteen.controller.js
+++ client-ng1/src/app/js/mixteen/mixteen.controller.js
@@ -7,10 +7,7 @@
 
     var ctrl = this;
 
-    //    Mireille Delpui : mireille.delpui@gmail.com
-    //    Francisco Nogueira : nogueira.frma@yahoo.fr
-
-    $http.post('/api/member/byids', [13,701, 4111, 488, 5582, 14, 48, 33, 7562, 7422, 7722, 7762])
+    $http.post('/api/member/byids', [13,701, 4111, 488, 5582, 14, 48, 33, 7562, 7422, 7722, 7762, 7842])
         .then(function (response) {
           ctrl.members = response.data;
         });
