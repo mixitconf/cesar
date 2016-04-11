8cf6ed80f60c43d9b286d53d42302d7f3cfae2c0
Guillaume EHRET <guillaume@dev-mind.fr>
2016-04-11 09:03:21 +0200

    Change style on transversal sessions in planning

--- client-ng1/src/app/js/planning/planning-tab-elt.directive.js
+++ client-ng1/src/app/js/planning/planning-tab-elt.directive.js
@@ -11,17 +11,39 @@
         timeslots: '=',
         rooms: '=',
         display: '=',
-        slots: '='
+        slots: '=',
+        day: '@'
       },
       controllerAs: 'ctrl',
       controller : function(){
         var ctrl = this;
 
-        ctrl.computeHeight = function(slot){
-          if(slot.duration>60){
-            return (slot.duration - 5)*2.0  + 'px';
+        ctrl.computeHeight = function(slot, transverse){
+          return transverse ? (slot.duration)*2.0 + 1  + 'px' : (slot.duration)*2.0  + 'px';
+        };
+
+        ctrl.isTransversalSlot = function(slot, room){
+          if(!!room && !!slot.room && slot.room.key==='Salle7' &&  (!!slot.label || !!slot.session)){
+            if (ctrl.display.amphi) {
+              return room.key === 'Amphi1';
+            }
+            if (ctrl.display.amphi) {
+              return room.key === 'Salle1';
+            }
+            if (ctrl.display.mezzanine) {
+              return room.key === 'Salle6';
+            }
           }
-          return (slot.duration - 2)*2.0  + 'px';
+          return false;
+        };
+
+        ctrl.computeWidth = function(slot, room){
+          if( ctrl.isTransversalSlot(slot, room)){
+            return (document.getElementById('planning1').offsetWidth -
+              document.getElementById('planninghour1').offsetWidth - 5) +
+              'px';
+          }
+          return 'auto';
         };
 
         ctrl.displayRoom = function (room) {
