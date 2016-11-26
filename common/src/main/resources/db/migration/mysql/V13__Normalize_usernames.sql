f84c7004558646a6ddbbe337deadc15f88b289ed
Sebastien Deleuze <seb@deleuze.fr>
2016-11-26 17:07:30 +0100

    Fix table names in SQL migration scripts

--- common/src/main/resources/db/migration/mysql/V13__Normalize_usernames.sql
+++ common/src/main/resources/db/migration/mysql/V13__Normalize_usernames.sql
@@ -1,5 +1,5 @@
-UPDATE account SET lastname=UCASE(lastname);
-UPDATE member SET lastname=UCASE(lastname);
+UPDATE Account SET lastname=UCASE(lastname);
+UPDATE Member SET lastname=UCASE(lastname);
 
-UPDATE account SET firstname =  CONCAT(UCASE(SUBSTRING(firstname,1,1)),'', LCASE(SUBSTRING(firstname,2,LENGTH(firstname))));
-UPDATE member SET firstname =  CONCAT(UCASE(SUBSTRING(firstname,1,1)),'', LCASE(SUBSTRING(firstname,2,LENGTH(firstname))));
+UPDATE Account SET firstname =  CONCAT(UCASE(SUBSTRING(firstname,1,1)),'', LCASE(SUBSTRING(firstname,2,LENGTH(firstname))));
+UPDATE Member SET firstname =  CONCAT(UCASE(SUBSTRING(firstname,1,1)),'', LCASE(SUBSTRING(firstname,2,LENGTH(firstname))));
